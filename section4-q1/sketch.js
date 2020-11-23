8c0b8ca405d8aeaa493af1fcbec2778b22b2e14e
Mitsuisohei <trackandfield2589@gmail.com>
2020-11-24 00:42:25 +0900

    訂正

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -11,8 +11,8 @@ function setup(){
 function draw(){
   background(252, 226, 196);
   count = (count + 1.5) % cycle;
-  if(count > 0 && count < cycle / 2){size = 40 + count;}
-  if(count >= cycle / 2 && count < cycle){size = cycle + 40 - count;}
+  if(count < cycle / 2){size = 40 + count;}
+  else{size = cycle + 40 - count;}
   //BLANK[1]
  fill(233, 84, 107);
  ellipse(width / 2, height / 2, size);
