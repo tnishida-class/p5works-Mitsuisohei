2fcdaee2966d40d8b992f7dc2aa3dfd3ff30cfdd
Mitsuisohei <trackandfield2589@gmail.com>
2020-11-23 16:40:10 +0900

    4-2q 回答

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,16 +1,22 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy, a;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  a = 10;
+  vx = 5;
+  vy = 5;
 }
 
 function draw(){
-  background(160, 192, 255);
+  background(32, 47, 85);
+  fill(255, 243, 82);
+  star(x, y, 40, a);
+  a += 5;
+  x += vx;
+  y += vy;
   // BLANK[2] (hint: 作った star 関数を使います)
 
   // 端の処理パターン (1) 反対側から出てくる
@@ -18,12 +24,13 @@ function draw(){
   else if(x < 0){ x = width; }
   if(y > height){ y = 0; }
   if(y < 0){ y = height; }
-}
+ }
 
 function star(cx, cy, r, angle){
+  noStroke();
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
